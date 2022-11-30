90d94cde8348058d070aeb98a12d65d3dd412c98
Nallieheai <nallieh1001@gmail.com>
2022-11-30 13:09:53 +0100

    Computing total amount of students in class & formatting the file using Shift+Alt+F

--- section 3/example.c
+++ section 3/example.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    printf("Hello World!\n");
+int main()
+{
+    int boys = 15;
+    int girls = 12;
+    int students = boys + girls;
+
+    printf("There are %d students in the class!", students);
+
     return EXIT_SUCCESS;
-} 
+}
