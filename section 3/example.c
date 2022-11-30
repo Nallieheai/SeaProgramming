2d668354ebe872a5863834bcbb5aba80f5131de7
Nallieheai <nallieh1001@gmail.com>
2022-11-30 13:32:08 +0100

    Basics of data types & variables in C

--- section 3/example.c
+++ section 3/example.c
@@ -7,7 +7,9 @@ int main()
     int girls = 12;
     int students = boys + girls;
 
-    printf("There are %d students in the class!", students);
+    printf("There are %d boys in the class!\n", boys);
+    printf("There are %d girls in the class!\n", girls);
+    printf("There are %d students in the class!\n", students);
 
     return EXIT_SUCCESS;
 }
---
+++ section 4/example.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*
+        Introduction to data types and variables in C
+        8 Bits = 1 Byte
+        4 or 8 Bytes = 1 WORD
+
+        Data Type - Identifier - Value (Literal; Fixed value)
+        int         boys       = 15;
+
+        Data Type also decides how much space the variable will take in memory.
+        Variable naming rules: 
+            1. Only letters, upper and lower case, digits ranging from 0 to 9 and underscores.
+            2. The first character cant be a digit.
+            3. Avoid starting the name with an underscore.
+
+        Fundamental Data Types in C:
+        Numbers: 78, 3.14
+        Characters: 'A'
+
+        Derived Data Types in C:
+        Strings: "Hello World" (collection of characters)
+        Array: ['H', 'e', 'l', 'l', 'o'] (collection of data type, for example string)
+        Complex types: struct, pointers, etc.
+    */
+
+    return EXIT_SUCCESS; // EXIT_SUCCESS is the same as returning 0
+}
