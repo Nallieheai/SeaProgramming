af2be7b6cd988805483a4baf081dc46860aa3699
Nallieheai <nallieh1001@gmail.com>
2022-12-02 14:15:37 +0100

    Binary arithmetic operators

--- section 5/operators.c
+++ section 5/operators.c
@@ -1,7 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void calculator_int()
+{
+    int a, b, c;
+
+    printf("Enter operand A (int): ");
+    scanf("%d", &a);
+    printf("\n");
+
+    printf("Enter operand B (int): ");
+    scanf("%d", &b);
+    printf("\n");
+
+    c = a + b;
+    printf("C = A + B -> %d\n", c);
+
+    c = a - b;
+    printf("C = A - B -> %d\n", c);
+    
+    c = a * b;
+    printf("C = A * B -> %d\n", c);
+
+    c = a / b;
+    printf("C = A / B -> %d\n", c);
+
+    c = a + b;
+    printf("C = A + B -> %d\n", c);
+
+    c = a % b;
+    printf("C = A %% B -> %d\n", c);
+    
+}
+
+void calculator_float()
+{
+    float a, b, c;
+
+    printf("Enter operand A (float): ");
+    scanf("%f", &a);
+    printf("\n");
+
+    printf("Enter operand B (float): ");
+    scanf("%f", &b);
+    printf("\n");
+
+    c = a + b;
+    printf("C = A + B -> %f\n", c);
+
+    c = a - b;
+    printf("C = A - B -> %f\n", c);
+    
+    c = a * b;
+    printf("C = A * B -> %f\n", c);
+
+    c = a / b;
+    printf("C = A / B -> %f\n", c);
+
+    c = a + b;
+    printf("C = A + B -> %f\n", c);
+
+    c = (int) a % (int) b;
+    printf("C = A %% B -> %f\n", c);
+    
+}
+
 int main()
 {
+    // calculator_int();
+    calculator_float();
+
     return 0;
 }
