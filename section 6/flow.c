35ed23b4d98d2654075f61d4aac0f79d33a90a29
Nallieheai <nallieh1001@gmail.com>
2022-12-05 19:36:28 +0100

    Planetary alignment challenge

--- section 6/flow.c
+++ section 6/flow.c
@@ -24,8 +24,53 @@ void check_number()
     
 }
 
+void planetary_alignment()
+{
+    int a, b, c;
+
+    printf("Enter the duration of orbit A in days: ");
+    scanf("%d", &a);
+    printf("\n");
+
+    printf("Enter the duration of orbit B in days: ");
+    scanf("%d", &b);
+    printf("\n");
+
+    printf("Enter the duration of orbit C in days: ");
+    scanf("%d", &c);
+    printf("\n\n");
+
+    printf("A -> %5d [days]\n", a);
+    printf("B -> %5d [days]\n", b);
+    printf("C -> %5d [days]\n\n", c);
+
+    int max;
+    max = a > b ? a : b;
+    max = c > max ? c : max;
+
+    int lcm = -1;
+    const int MAX_ITER = 10000;
+
+    for (int i = 1; i <= MAX_ITER; i++)
+    {
+        int mul = max * i;
+        if (mul % a == 0 && mul % b == 0 && mul % c == 0) {
+            lcm = mul;
+            printf("Found LCM after %d iterations!\n", i);
+            printf("Orbits A, B and C align every %d days!\n", lcm);
+            printf("Planet A completes %d orbits.\n", lcm / a);
+            printf("Planet B completes %d orbits.\n", lcm / b);
+            printf("Planet C completes %d orbits.\n", lcm / c);
+            break;
+        }
+    }
+
+    if (lcm == -1) printf("Exceeded the maximum number of iterations!");
+}
+
 int main() 
 {
-    check_number();
+    // check_number();
+    planetary_alignment();
     return 0;
 }
