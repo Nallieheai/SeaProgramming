88f145adfe91199bd423fd36e27c79971e00ed84
Nallieheai <nallieh1001@gmail.com>
2022-12-09 13:39:11 +0100

    Function declaration in source files

--- section 7/functions.c
+++ section 7/functions.c
@@ -12,10 +12,24 @@
     int main() {
         return '0'; => return (int) '0';
     }
+
+    function decalaration can be used to make code easier to read
+    int sum(int a, int b);
+
+    // At this point the compiler wouldnt know what sum() is unless it was decalared before the main function
+    int main() { 
+        int retval = sum(5, 2);
+        return 0;
+    }
+
+    int sum(int a, int b) {
+        ...
+    }
 */
 
 int main() 
 {
 
+    sum();
     return 0;
 }
