f8a26efe0978271020c4d2265991d62ff46a2620
Nallieheai <nallieh1001@gmail.com>
2022-12-09 13:33:26 +0100

    Function basics

---
+++ section 7/functions.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    A function can be used to execute an action, e.g. printf("abc")
+    A function can be used to obtain a result, e.g. letter = toLower('A')
+    A function can be used to change something, e.g. increment(&counter) 
+
+    return type at the end of the function is automatically casted to the return type
+    example: 
+
+    int main() {
+        return '0'; => return (int) '0';
+    }
+*/
+
+int main() 
+{
+
+    return 0;
+}
