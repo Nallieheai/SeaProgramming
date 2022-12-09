fcab5e1541d90bbd8b29fc6b5ba31ea277284700
Nallieheai <nallieh1001@gmail.com>
2022-12-09 13:49:35 +0100

    Scopes & rules

--- section 7/functions.c
+++ section 7/functions.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 /*
     A function can be used to execute an action, e.g. printf("abc")
@@ -29,7 +30,23 @@
 
 int main() 
 {
+    int a = 5;
 
-    sum();
+    if (true) {
+        a += 5;
+        printf("The value of a is: %d\n", a);
+
+        int b = 4;
+    }
+
+    // This line of code cant be executed becasue the variable b is
+    // initialized inside of the if-statement, therefore it is outside
+    // of our current scope.
+    // printf("The value of a is: %d\n", b);
+
+    // Visibility rules:
+    // A local variable hides a global variable
+    // A block variable hides local & global variables
+
     return 0;
 }
